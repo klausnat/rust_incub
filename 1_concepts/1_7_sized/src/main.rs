66a16bfbde7a94e3f8ca249f61b9725f26aab470
Natasha <webmathew@gmail.com>
2025-07-22 14:35:37 +0300

    wip

--- 1_concepts/1_7_sized/src/main.rs
+++ 1_concepts/1_7_sized/src/main.rs
@@ -1,15 +1,13 @@
 // TASK
-// Given the User and UserRepository implementations from the previous task, write the actual code 
+// Given the User and UserRepository implementations from the previous task, write the actual code
 // for CommandHandler<CreateUser> implementation described above.
 
-// Provide tests for CommandHandler<CreateUser> implementation where dyn UserRepository is mocked 
+// Provide tests for CommandHandler<CreateUser> implementation where dyn UserRepository is mocked
 // with another hand-written type for testing purposes (you will need to transform the UserRepository type into a trait).
 
-use step_1_7;
-use step_1_6::*;
-use std::process::Command;
+use std::{borrow::Cow, collections::HashMap};
+use step_1_7::*;
 
 fn main() {
     
-
 }
