f7f6b5e3970c116e020b3387c194db823663b18b
Natasha <webmathew@gmail.com>
2025-07-29 17:43:36 +0300

    first version of implementation of len and insert_after

--- 1_concepts/src/lib.rs
+++ 1_concepts/src/lib.rs
@@ -1,18 +1,18 @@
 use std::sync::{Arc, Mutex, Weak};
 
-#[derive(Debug)]
-pub struct Node<T> {
+#[derive(Debug, Clone)]
+pub struct Node<T: Clone + Copy> {
     r_previous: Option<Weak<Mutex<Node<T>>>>,
     r_next: Option<Arc<Mutex<Node<T>>>>,
     data: T,
 }
 
-pub struct List<T> {
+pub struct List<T: Clone + Copy> {
     first: Arc<Mutex<Node<T>>>,
     last: Arc<Mutex<Node<T>>>,
 }
 
-impl<T> List<T> {
+impl<T: Clone + Copy> List<T> {
     // constructor
     pub fn new(data1: T, data2: T) -> Self {
         // create 2 nodes
@@ -39,8 +39,27 @@ impl<T> List<T> {
         List { first, last }
     }
 
-    // traverse (apply function to each element)
-    // Simple traverse method that applies a function to each element
+    pub fn len(&self) -> i32 {
+        let mut len = 0;
+        let mut res = Arc::clone(&self.first);
+        loop {
+            let next = {
+                let mut node = res.lock().unwrap();
+                len += 1;
+                node.r_next.clone()
+            };
+
+            if let Some(next) = next {
+                res = next
+            } else {
+                break;
+            }
+        }
+        len
+    }
+
+    // Traverse (apply function to each element)
+    // allows mutation during traversal
     pub fn traverse<F>(&self, mut f: F)
     where
         F: FnMut(&mut T),
@@ -60,6 +79,17 @@ impl<T> List<T> {
             }
         }
     }
+
+    // insert new node after specific node
+    pub fn insert_after(&mut self, mut node: Node<T>, mut new_node: Node<T>) {
+        node.r_next = Some(Arc::new(Mutex::new(new_node.clone())));
+        let save_prev = new_node.r_previous.clone();
+        new_node.r_previous = Some(Arc::downgrade(&Arc::new(Mutex::new(node))));
+        match save_prev {
+            None => new_node.r_next = None,
+            _ => new_node.r_next = Some(save_prev.unwrap().upgrade().unwrap()),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -77,6 +107,7 @@ mod tests {
             (2, 3)
         );
     }
+
     #[test]
     fn traverse_mut_list() {
         let list = List::new(2, 3);
@@ -88,5 +119,21 @@ mod tests {
             ),
             (3, 4)
         );
+    }
+
+    #[test]
+    fn length() {
+        let list = List::new(2, 3);
+
+        assert_eq!(list.len(), 2);
+
+        let new_node = Node {
+            r_previous: None,
+            r_next: None,
+            data: 5,
+        };
+
+        list.insert_after(self.first, new_node);
+        assert_eq!(list.len(), 3);
     }
 }
