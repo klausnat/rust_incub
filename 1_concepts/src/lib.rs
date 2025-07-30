ea886c4a30a582e5ce0812b846b0d336fde8e566
Natasha <webmathew@gmail.com>
2025-07-30 15:24:17 +0300

    fixed function insert_after, remove_node fn created

--- 1_concepts/src/lib.rs
+++ 1_concepts/src/lib.rs
@@ -81,14 +81,53 @@ impl<T: Clone + Copy> List<T> {
     }
 
     // insert new node after specific node
-    pub fn insert_after(&mut self, mut node: Node<T>, mut new_node: Node<T>) {
-        node.r_next = Some(Arc::new(Mutex::new(new_node.clone())));
-        let save_prev = new_node.r_previous.clone();
-        new_node.r_previous = Some(Arc::downgrade(&Arc::new(Mutex::new(node))));
-        match save_prev {
-            None => new_node.r_next = None,
-            _ => new_node.r_next = Some(save_prev.unwrap().upgrade().unwrap()),
+    pub fn insert_after(&self, existing_node: &Arc<Mutex<Node<T>>>, new_data: T) {
+        // create new node
+        let new_node = Arc::new(Mutex::new(Node {
+            r_previous: Some(Arc::downgrade(existing_node)),
+            r_next: None,
+            data: new_data,
+        }));
+
+        let mut existing = existing_node.lock().unwrap();
+
+        // store the old next node
+        let old_next = existing.r_next.take();
+
+        // link the new node to the old_next node if it exists
+        if let Some(ref next_node) = old_next {
+            let mut next = next_node.lock().unwrap();
+            next.r_previous = Some(Arc::downgrade(&new_node));
         }
+
+        existing.r_next = Some(Arc::clone(&new_node));
+
+        new_node.lock().unwrap().r_next = old_next;
+    }
+
+    // remove node
+    // Remove a node identified by its Arc<Mutex<Node<T>>>
+    pub fn remove_node(&self, node_to_remove: &Arc<Mutex<Node<T>>>) -> Result<(), &'static str> {
+        // Get the node's previous and next pointers
+        let (prev, next) = {
+            let node = node_to_remove.lock().unwrap();
+            (node.r_previous.clone(), node.r_next.clone())
+        };
+
+        // Update previous node's next pointer if it exists
+        if let Some(prev_weak) = prev {
+            if let Some(prev_node) = prev_weak.upgrade() {
+                let mut prev = prev_node.lock().unwrap();
+                prev.r_next = next.clone();
+            }
+        }
+        // Update next node's previous pointer if it exists
+        else if let Some(next_node) = next {
+            let mut next = next_node.lock().unwrap();
+            next.r_previous = prev;
+        }
+
+        Ok(())
     }
 }
 
@@ -122,18 +161,45 @@ mod tests {
     }
 
     #[test]
-    fn length() {
-        let list = List::new(2, 3);
+    fn length_and_insert_after() {
+        let mut list = List::new(2, 3);
 
         assert_eq!(list.len(), 2);
 
-        let new_node = Node {
-            r_previous: None,
-            r_next: None,
-            data: 5,
+        let node = Arc::clone(&list.first);
+
+        list.insert_after(&node, 5);
+
+        assert_eq!(list.len(), 3);
+
+        // Verify the order
+        let mut items = Vec::new();
+        list.traverse(|x| items.push(*x));
+        assert_eq!(items, vec![2, 5, 3]);
+    }
+
+    #[test]
+    fn test_remove_node() {
+        let list = List::new(2, 3);
+
+        // Insert a node to make list: 2 -> 5 -> 3
+        let first_node = Arc::clone(&list.first);
+        list.insert_after(&first_node, 5);
+        assert_eq!(list.len(), 3);
+
+        // Get the middle node (5)
+        let middle_node = {
+            let first = first_node.lock().unwrap();
+            first.r_next.as_ref().unwrap().clone()
         };
 
-        list.insert_after(self.first, new_node);
-        assert_eq!(list.len(), 3);
+        // Remove the middle node
+        list.remove_node(&middle_node).unwrap();
+        assert_eq!(list.len(), 2);
+
+        // Verify list is now 2 -> 3
+        let mut items = Vec::new();
+        list.traverse(|x| items.push(*x));
+        assert_eq!(items, vec![2, 3]);
     }
 }
