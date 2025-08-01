f40c2e6cea283dea5227c6e8195c1f29aa0080e4
Natasha <webmathew@gmail.com>
2025-08-01 16:30:50 +0300

    wip

--- 2_idioms/2_1_type_safety/src/main.rs
+++ 2_idioms/2_1_type_safety/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Implement me!");
+mod post {
+    #[derive(Clone, Debug, PartialEq)]
+    pub struct Id(u64);
+
+    #[derive(Clone, Debug, PartialEq)]
+    pub struct Title(String);
+
+    #[derive(Clone, Debug, PartialEq)]
+    pub struct Body(String);
+
+    // A series of zero-sized types representing the state of the post
+    struct ReadyForModeration;
+    struct Unmoderated;
+    struct Deleted;
+    struct Published;
+
+    
+    #[derive(Clone, Debug, PartialEq)]
+    pub enum State {
+        ReadyForModeration,
+        Unmoderated,
+        Deleted,
+        Published,
+    }
 }
+mod user {
+    #[derive(Clone, Debug, PartialEq)]
+    pub struct Id(u64);
+}
+
+#[derive(Clone)]
+struct Post {
+    id: post::Id,
+    user_id: user::Id,
+    title: post::Title,
+    body: post::Body,
+    state: post::State,
+}
+
+fn main() {}
